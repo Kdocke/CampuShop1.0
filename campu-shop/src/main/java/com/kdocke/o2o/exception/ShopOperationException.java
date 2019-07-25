d4a2c268966c846b336d19818e9db7ae622f1f21
Kdocke <kdocked@gmail.com>
2019-07-25 23:58:49 +0800

    Update:增加部分注释

--- campu-shop/src/main/java/com/kdocke/o2o/exception/ShopOperationException.java
+++ campu-shop/src/main/java/com/kdocke/o2o/exception/ShopOperationException.java
@@ -1,5 +1,11 @@
 package com.kdocke.o2o.exception;
 
+/**
+ * Shop 操作异常类
+ * 
+ * @author Kdocke
+ *
+ */
 public class ShopOperationException extends RuntimeException {
 
 	private static final long serialVersionUID = 1L;
@@ -7,5 +13,5 @@ public class ShopOperationException extends RuntimeException {
 	public ShopOperationException(String msg) {
 		super(msg);
 	}
-	
+
 }
