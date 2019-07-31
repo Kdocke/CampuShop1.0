90eb0ce67eac856bcaab5a1c2c02ed0fc7d809c8
Kdocke <kdocked@gmail.com>
2019-07-31 21:59:27 +0800

    Add:店铺列表展示之 Dao 层实现与测试

--- campu-shop/src/main/java/com/kdocke/o2o/dao/ShopDao.java
+++ campu-shop/src/main/java/com/kdocke/o2o/dao/ShopDao.java
@@ -1,11 +1,13 @@
 package com.kdocke.o2o.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
 import com.kdocke.o2o.entity.Shop;
 
 /**
- * 商铺相关的数据库操作接口，由 mybatis 实现
- * queryByShopId:查询店铺信息
- * insertShop:向数据库中插入一条店铺信息
+ * 商铺相关的数据库操作接口，由 mybatis 实现 queryByShopId:查询店铺信息 insertShop:向数据库中插入一条店铺信息
  * updateShop:更新数据库中的一条店铺信息
  * 
  * @author Kdocke
@@ -14,21 +16,46 @@ import com.kdocke.o2o.entity.Shop;
 public interface ShopDao {
 
 	/**
+	 * 分页查询店铺,可输入的条件有:店铺名(模糊),店铺状态,店铺类别,区域Id,owner
+	 * 
+	 * @param shopCondition
+	 *            条件
+	 * @param rowIndex
+	 *            从第几行开始取数据
+	 * @param pageSize
+	 *            返回的条数
+	 * @return
+	 */
+	List<Shop> queryShopList(@Param("shopCondition") Shop shopCondition, @Param("rowIndex") int rowIndex,
+			@Param("pageSize") int pageSize);
+
+	/**
+	 * 返回 queryShopList 总数,用于分页
+	 * 
+	 * @param shopCondition
+	 * @return
+	 */
+	int queryShopCount(@Param("shopCondition") Shop shopCondition);
+
+	/**
 	 * 通过 shop id 查询店铺
+	 * 
 	 * @param shopId
 	 * @return
 	 */
 	Shop queryByShopId(long shopId);
-	
+
 	/**
 	 * 新增店铺
+	 * 
 	 * @param shop
 	 * @return
 	 */
 	int insertShop(Shop shop);
-	
+
 	/**
 	 * 更新店铺信息
+	 * 
 	 * @param shop
 	 * @return
 	 */
