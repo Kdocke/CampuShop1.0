bd35b24d3ebe9394b0d7dd39d7eaa927f0dcc8c8
Kdocke <kdocked@gmail.com>
2019-08-02 11:27:51 +0800

    Add:商品类别批量添加 Dao 层开发与测试

--- campu-shop/src/test/java/com/kdocke/o2o/dao/ProductCategoryDaoTest.java
+++ campu-shop/src/test/java/com/kdocke/o2o/dao/ProductCategoryDaoTest.java
@@ -1,7 +1,12 @@
 package com.kdocke.o2o.dao;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -13,11 +18,31 @@ public class ProductCategoryDaoTest extends BaseTest {
 	@Autowired
 	private ProductCategoryDao productCategoryDao;
 	
+	@Ignore
 	@Test
 	public void testQueryByShopId() {
 		long shopId = 1;
 		List<ProductCategory> productCategoryList = productCategoryDao.queryProductCategoryList(shopId);
 		System.out.println("该店铺自定义类别数为: " + productCategoryList.size());
+	}
+	
+	@Test
+	public void testbatchInsertProductCategory() {
+		ProductCategory productCategory1 = new ProductCategory();
+		productCategory1.setProductCategoryName("商品类别1");
+		productCategory1.setPriority(1);
+		productCategory1.setCreateTime(new Date());
+		productCategory1.setShopId(1L);
+		ProductCategory productCategory2 = new ProductCategory();
+		productCategory2.setProductCategoryName("商品类别2");
+		productCategory2.setPriority(2);
+		productCategory2.setCreateTime(new Date());
+		productCategory2.setShopId(1L);
+		List<ProductCategory> productCategoryList = new ArrayList<ProductCategory>();
+		productCategoryList.add(productCategory1);
+		productCategoryList.add(productCategory2);
+		int effectedNum = productCategoryDao.batchInsertProductCategory(productCategoryList);
+		assertEquals(2, effectedNum);
 	}
 	
 }
