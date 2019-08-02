e29f56e3cc7efee112bab90cbf057981f6968f76
Kdocke <kdocked@gmail.com>
2019-08-02 12:29:13 +0800

    Add:商品类别批量添加前端的开发

--- campu-shop/src/main/webapp/resources/js/shop/productcategorymanagement.js
+++ campu-shop/src/main/webapp/resources/js/shop/productcategorymanagement.js
@@ -1,34 +1,10 @@
 $(function() {
 	var shopId = 1;
 	var listUrl = '/campu-shop/shopadmin/getproductcategorylist';
-	var addUrl = '/campu-shop/shopadmin/addproductcategorys';
+	var addUrl = '/campu-shop/shopadmin/addproductcategories';
 	var deleteUrl = '/campu-shop/shopadmin/removeproductcategory';
 
-	$.getJSON(
-					listUrl,
-					function(data) {
-						if (data.success) {
-							var dataList = data.data;
-							$('.category-wrap').html('');
-							var tempHtml = '';
-							dataList
-									.map(function(item, index) {
-										tempHtml += ''
-												+ '<div class="row row-product-category now">'
-												+ '<div class="col-33 product-category-name">'
-												+ item.productCategoryName
-												+ '</div>'
-												+ '<div class="col-33">'
-												+ item.priority
-												+ '</div>'
-												+ '<div class="col-33"><a href="#" class="button delete" data-id="'
-												+ item.productCategoryId
-												+ '">删除</a></div>' + '</div>';
-									});
-							$('.category-wrap').append(tempHtml);
-						}
-					});
-
+	// 列表显示商品分类
 	getList();
 	function getList() {
 		$.getJSON(listUrl, function(data) {
@@ -54,8 +30,19 @@ $(function() {
 			}
 		});
 	}
-
-
+	
+	// 新增一条商品分类
+	$('#new').click(
+		function() {
+			var tempHtml = '<div class="row row-product-category temp">'
+					+ '<div class="col-33"><input class="category-input category" type="text" placeholder="分类名"></div>'
+					+ '<div class="col-33"><input class="category-input priority" type="number" placeholder="优先级"></div>'
+					+ '<div class="col-33"><a href="#" class="button delete">删除</a></div>'
+					+ '</div>';
+			$('.category-wrap').append(tempHtml);
+		});
+	
+	// 提交新增的商品分类
 	$('#submit').click(function() {
 		var tempArr = $('.temp');
 		var productCategoryList = [];
@@ -75,6 +62,7 @@ $(function() {
 			success : function(data) {
 				if (data.success) {
 					$.toast('提交成功！');
+					// 重新渲染页面
 					getList();
 				} else {
 					$.toast('提交失败！');
@@ -82,17 +70,33 @@ $(function() {
 			}
 		});
 	});
-
-	$('#new')
-			.click(
-					function() {
-						var tempHtml = '<div class="row row-product-category temp">'
-								+ '<div class="col-33"><input class="category-input category" type="text" placeholder="分类名"></div>'
-								+ '<div class="col-33"><input class="category-input priority" type="number" placeholder="优先级"></div>'
-								+ '<div class="col-33"><a href="#" class="button delete">删除</a></div>'
-								+ '</div>';
-						$('.category-wrap').append(tempHtml);
-					});
+	
+	$.getJSON(
+			listUrl,
+			function(data) {
+				if (data.success) {
+					var dataList = data.data;
+					$('.category-wrap').html('');
+					var tempHtml = '';
+					dataList
+							.map(function(item, index) {
+								tempHtml += ''
+										+ '<div class="row row-product-category now">'
+										+ '<div class="col-33 product-category-name">'
+										+ item.productCategoryName
+										+ '</div>'
+										+ '<div class="col-33">'
+										+ item.priority
+										+ '</div>'
+										+ '<div class="col-33"><a href="#" class="button delete" data-id="'
+										+ item.productCategoryId
+										+ '">删除</a></div>' + '</div>';
+							});
+					$('.category-wrap').append(tempHtml);
+				}
+			});
+	
+	
 
 	$('.category-wrap').on('click', '.row-product-category.now .delete',
 			function(e) {
