054d0c5dca794c1b10452610c471622bdff20570
Kdocke <kdocked@gmail.com>
2019-08-06 22:49:34 +0800

    Add:商品添加之前端的开发与测试

--- campu-shop/src/main/java/com/kdocke/o2o/web/shopadmin/ShopAdminController.java
+++ campu-shop/src/main/java/com/kdocke/o2o/web/shopadmin/ShopAdminController.java
@@ -4,28 +4,43 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+/**
+ * 主要用来解析路由并转发到相应的 html 中
+ * 
+ * @author Kdocke
+ */
 @Controller
-@RequestMapping(value="shopadmin", method={RequestMethod.GET})
+@RequestMapping(value = "shopadmin", method = { RequestMethod.GET })
 public class ShopAdminController {
 
-	@RequestMapping(value="/shopoperation")
+	@RequestMapping(value = "/shopoperation")
 	public String shopOperation() {
+		// 转发到店铺注册/编辑页面
 		return "shop/shopoperation";
 	}
-	
-	@RequestMapping(value="/shoplist")
+
+	@RequestMapping(value = "/shoplist")
 	public String shopList() {
+		// 转发到店铺列表页面
 		return "shop/shoplist";
 	}
-	
-	@RequestMapping(value="/shopmanagement")
+
+	@RequestMapping(value = "/shopmanagement")
 	public String shopManagement() {
+		// 转发到店铺管理页面
 		return "shop/shopmanagement";
 	}
-	
-	@RequestMapping(value="/productcategorymanagement", method=RequestMethod.GET)
+
+	@RequestMapping(value = "/productcategorymanagement", method = RequestMethod.GET)
 	public String procuctCategoryManagement() {
+		// 转发到商品类别管理页面
 		return "shop/productcategorymanagement";
 	}
 	
+	@RequestMapping(value = "/productoperation")
+	public String productOperation() {
+		// 转发到商品添加/编辑页面
+		return "shop/productoperation";
+	}
+
 }
