7ba3215b220c41a2f1ad112d8cd254ac8f8640be
Kdocke <kdocked@gmail.com>
2019-08-17 23:56:10 +0800

    Add:HeadLine 之 Service 层的开发

--- campu-shop/src/main/java/com/kdocke/o2o/service/ShopCategoryService.java
+++ campu-shop/src/main/java/com/kdocke/o2o/service/ShopCategoryService.java
@@ -5,7 +5,13 @@ import java.util.List;
 import com.kdocke.o2o.entity.ShopCategory;
 
 public interface ShopCategoryService {
-	
+
+	/**
+	 * 根据查询条件获取 shopCategory 列表
+	 * 
+	 * @param shopCategoryCondition
+	 * @return
+	 */
 	List<ShopCategory> getShopCategoryList(ShopCategory shopCategoryCondition);
-	
+
 }
---
+++ campu-shop/src/main/java/com/kdocke/o2o/service/impl/HeadLineServiceImpl.java
@@ -0,0 +1,23 @@
+package com.kdocke.o2o.service.impl;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.kdocke.o2o.dao.HeadLineDao;
+import com.kdocke.o2o.entity.HeadLine;
+import com.kdocke.o2o.service.HeadLineService;
+
+public class HeadLineServiceImpl implements HeadLineService {
+
+	@Autowired
+	private HeadLineDao headLineDao;
+
+	@Override
+	public List<HeadLine> getHeadLineList(HeadLine headLineCondition) throws IOException {
+		return headLineDao.queryHeadLine(headLineCondition);
+	}
+
+
+}
