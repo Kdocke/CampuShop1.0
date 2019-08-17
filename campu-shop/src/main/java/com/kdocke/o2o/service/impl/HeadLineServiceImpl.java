7f02e8e6dc8685153030554bef3c3190dffffc1e
Kdocke <kdocked@gmail.com>
2019-08-18 00:18:39 +0800

    Add:MainPageController 之 listMainPageInfo 的开发

--- campu-shop/src/main/java/com/kdocke/o2o/service/impl/HeadLineServiceImpl.java
+++ campu-shop/src/main/java/com/kdocke/o2o/service/impl/HeadLineServiceImpl.java
@@ -4,11 +4,13 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import com.kdocke.o2o.dao.HeadLineDao;
 import com.kdocke.o2o.entity.HeadLine;
 import com.kdocke.o2o.service.HeadLineService;
 
+@Service
 public class HeadLineServiceImpl implements HeadLineService {
 
 	@Autowired
